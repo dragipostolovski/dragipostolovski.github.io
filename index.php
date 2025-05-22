ea62c2c6f2e2389109fb55288fd6d1545e0333ee
Dragi Postolovski <dpostolovskimk@gmail.com>
2025-05-22 12:06:08 +0200

    rotator

--- index.php
+++
@@ -1,8 +0,0 @@
-
-<?php include 'inc/header.php';?>
-  <main class="site-main">
-    <?php include 'inc/rotate.php'; ?>
-    <?php include 'inc/slider.php'; ?>
-    <?php include 'inc/grid.php'; ?>
-  </main>
-<?php include 'inc/footer.php';?>
